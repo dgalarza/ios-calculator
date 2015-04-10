5822bf361c32086cfbd4b5ce27628fcd2d98fa08
Damian Galarza <damian@thoughtbot.com>
2015-04-10 19:40:27 -0400

    Basic calculation functionality

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,10 +12,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     
     var userIsTyping: Bool = false
+    var operands = [Double]()
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        
+        set {
+            println(newValue)
+            display.text = "\(newValue)"
+            userIsTyping = false
+        }
+    }
     
     @IBAction func inputDigit(sender: UIButton) {
         let digit = sender.currentTitle!
-
+        
         if userIsTyping {
             display.text = display.text! + digit
         } else {
@@ -27,6 +40,34 @@ class ViewController: UIViewController {
     @IBAction func reset() {
         userIsTyping = false
         display.text = "0"
+        operands = [Double]()
+    }
+    
+    @IBAction func enter() {
+        operands.append(displayValue)
+        userIsTyping = false
+    }
+    
+    @IBAction func calculate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        
+        if userIsTyping {
+            enter()
+        }
+        
+        switch operation {
+        case "✖️": performCalculation { $0 * $1 }
+        case "➗": performCalculation { $1 / $0 }
+        case "➕": performCalculation { $0 + $1 }
+        case "➖": performCalculation { $1 - $0 }
+        default: enter()
+        }
+    }
+    
+    func performCalculation(calculation: (Double, Double) -> Double) {
+        if operands.count >= 2 {
+            displayValue = calculation(operands.removeLast(), operands.removeLast())
+            enter()
+        }
     }
 }
-
